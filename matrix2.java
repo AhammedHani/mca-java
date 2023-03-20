4238659c1e84f6a936b64f1f0553462ecc1c72dc
Ahammed Hani <105771459+AhammedHani@users.noreply.github.com>
2023-03-20 21:30:28 +0530

    S2

--- matrix2.java
+++ matrix2.java
@@ -13,7 +13,6 @@ class TransposeofMatrix{
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 arr1[i][j]=sc.nextInt();
-
             }
         }
         System.out.println("matrix is:");
